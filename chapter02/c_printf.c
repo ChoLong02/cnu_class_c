2f4438d759e9346e06eaffb4dda8b7af6ecf9707
Chulwoong Choi <sentilemon02@gmail.com>
2022-11-02 10:39:11 +0900

    Add files via upload

---
+++ chapter02/c_printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// 제어 문자
+//   + \n : 한 줄 개행
+//   + \t : 탭(8칸 띄움)
+int main(void) {
+    printf("Hello");
+    printf("C\n");
+
+    printf("==================================\n");
+    pritnf("Be happy\n");
+    printf("My\tfriend\n");
+
+    printf("==================================\n");
+    
+}
