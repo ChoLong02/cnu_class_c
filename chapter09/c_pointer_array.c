bbe40513ba875c5d72ec7854e3c9230b38194bb1
Chulwoong Choi <sentilemon02@gmail.com>
2022-12-14 10:09:35 +0900

    Add files via upload

---
+++ chapter09/c_pointer_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * 포인터(Pointer)와 배열(Array)
+ *  - 
+*/
+
+int main(void) {
+    int ary[3];  // 배열 선언(3칸) 
+    int i;       // 변수           
+
+    // 배열 이름 → 배열의 시작번지 값을 담고있음
+    *(ary + 0) = 10;
+    *(ary + 1) = *(ary + 0) + 10;
+
+    printf("3번째 배열 요소 입력: ");
+    scanf("%d", ary + 2);
+    
+    for(i=0; i<3; i++) {
+        printf("%d\n", *(ary+i));
+    }
+
+
+
+
+}
